417f70c6114f46ca0ec4c70f1686f2676b2302f6
nikolaienko19 <stiebastia@gmail.com>
2024-11-29 22:04:23 +0200

    goooo

--- main.js
+++ main.js
@@ -1,95 +1,18 @@
 
-
-// const dayButton = document.querySelector(".dayBlock")
-// const modal = document.querySelector(".surprise")
-// const closeBtn = document.querySelector(".close")
-
-// document.addEventListener("DOMContentLoaded",() => {
-//   dayButton.addEventListener("click", handleClick)
-// })
-
-// function handleClick(event) {
-//   modal.style.display = "block";
-//   closeBtn.addEventListener("click", () => {
-//     modal.style.display = "none"
-//   })
-// }
-
-
-// function replaceDayBlock(doneTasks) {
-//   // Перевіряємо, чи масив містить число 1
-//   if (doneTasks.includes(1)) {
-//     // Знаходимо div з класами "dayBlock" і "day1"
-//     const originalDiv = document.querySelector('.day1');
-
-//     if (originalDiv) {
-//       // Створюємо новий div
-//       const newDiv = document.createElement('div');
-//       newDiv.classList.add('dayDone','day1done');
-
-//       // Додаємо вміст до нового div
-//       const img = document.createElement('img');
-//       img.id = 'meme-img';
-//       img.src = 'images/cat.png';
-//         img.alt = '';
-//         img.width = 150;
-//         img.height = 150;
-//       newDiv.appendChild(img);
-
-//       // Замінюємо старий div на новий
-//       originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-//     }
-//     }
-//     if (doneTasks.includes(2)) {
-//     // Знаходимо div з класами "dayBlock" і "day1"
-//     const originalDiv = document.querySelector('.day2');
-
-//     if (originalDiv) {
-//       // Створюємо новий div
-//       const newDiv = document.createElement('div');
-//       newDiv.classList.add('dayDone','day2done');
-
-//       // Додаємо вміст до нового div
-//       const img = document.createElement('img');
-//       img.id = 'meme-img';
-//       img.src = 'images/cat.png';
-//         img.alt = '';
-//         img.width = 150;
-//         img.height = 150;
-//       newDiv.appendChild(img);
-
-//       // Замінюємо старий div на новий
-//       originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-//     }
-//     }
-//     if (doneTasks.includes(3)) {
-//     // Знаходимо div з класами "dayBlock" і "day1"
-//     const originalDiv = document.querySelector('.day3');
-
-//     if (originalDiv) {
-//       // Створюємо новий div
-//       const newDiv = document.createElement('div');
-//       newDiv.classList.add('dayDone','day3done');
-
-//       // Додаємо вміст до нового div
-//       const img = document.createElement('img');
-//       img.id = 'meme-img';
-//       img.src = 'images/cat.png';
-//         img.alt = '';
-//         img.width = 150;
-//         img.height = 150;
-//       newDiv.appendChild(img);
-
-//       // Замінюємо старий div на новий
-//       originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-//     }
-//   }
-// }
-
-
-// // // Виклик функції з масивом doneTasks
-// // const doneTasks = [1, 2, 3]; // Якщо число 1 є у масиві, заміна відбудеться
-// replaceDayBlock(doneTasks);
+const tasks = [
+  "Сфотографуй себе або свою домашню тваринку на фоні ялинки!",       // Завдання для дня 20
+  "Напиши жартівливого листа Санті! На папері, з малюночками, щоб все як в дитинстві:)",          // Завдання для дня 21
+  "Зроби картинку-мем про роботу або про колег (не образливий!)",  // Завдання для дня 22
+  "Заспівай Last Christmas та запиши на відео або аудіо!",     // Завдання для дня 23
+  "Скинь свою дитячу фотку з новорічного свята або в новорічному костюмі або... Коротше, давай так, щоб всі зрозуміли, до якого свята ти готуєшся!",// Завдання для дня 24
+  "Збудуй сніговика з будь-чого, але не зі снігу!",  // Завдання для дня 25
+  "Запакуй себе або частину тіла як новорічний подарунок, так як ти найкращий сюрприз на світі!",          // Завдання для дня 26
+  "Покажи, як ти їси свою найулюбленішу новорічну страву!", // Завдання для дня 27
+  "Скинь свою найулюбленішу фотку разом з колегою!",       // Завдання для дня 28
+  "Зроби новорічну поробку!", // Завдання для дня 29
+  "Порекомендуй, який новорічний фільм подивитись колегам?",           // Завдання для дня 31
+  "Поділись своєю улюбленою новорічною або різдвяною піснею!" // Завдання для дня 31
+];
 
 
 // 
@@ -115,42 +38,35 @@ $(document).ready(function () {
     if (todaysDate > day) {
       $(this).addClass('past');
     }
+
+    // Додаємо тексти завдань тільки для today і past
+    if (todaysDate >= day) {
+      const taskIndex = day - 1; // Індекс у масиві (починається з 0)
+      const taskText = tasks[taskIndex];
+      $(this).find('.modalText p').text(taskText); // Встановлюємо текст у <p>
+    }
   });
 
   // Відкриття відповідної модалки
   $('.dayBlock').click(function () {
     var day = parseInt($(this).find('#dayNumber').html()); // Читаємо число з <p id="dayNumber">
     
-    if (todaysDate > day) {
-      // Якщо дата в минулому, відкриваємо стандартну модалку
+    if ($(this).hasClass('past') || $(this).hasClass('today')) {
+      // Якщо дата в минулому або сьогодні, відкриваємо стандартну модалку із завданням
       $(this).find('.surprise-backdrop').addClass('open');
       $(this).find('.surprise').addClass('open');
-    } else if (todaysDate < day) {
-      // Якщо дата в майбутньому, відкриваємо модалку "Ти не гадалка"
+    } else if ($(this).hasClass('future')) {
+      // Якщо дата в майбутньому, відкриваємо модалку "Ти не ворожка"
       $(this).find('.futureModal-backdrop').addClass('open');
       $(this).find('.futureModal').addClass('open');
     }
   });
 
-  // Закриття стандартного модального вікна
-  $(document).on('click', '.surprise .close', function () {
-    $(this).closest('.surprise').removeClass('open'); // Закриваємо модалку
-    $(this).closest('.dayBlock').find('.surprise-backdrop').removeClass('open'); // Закриваємо backdrop
-  });
-
-  // Закриття модалки "Ти не гадалка"
-  $(document).on('click', '.futureModal .close', function () {
-    $(this).closest('.futureModal').removeClass('open'); // Закриваємо майбутню модалку
-    $(this).closest('.dayBlock').find('.futureModal-backdrop').removeClass('open'); // Закриваємо backdrop
-  });
-
-  // Закриття будь-якої модалки через backdrop
-  $(document).on('click', '.surprise-backdrop, .futureModal-backdrop', function () {
-    $(this).removeClass('open'); // Закриваємо backdrop
-    $(this).siblings('.surprise, .futureModal').removeClass('open'); // Закриваємо відповідну модалку
+  // Закриття модалок
+  $('.close').click(function () {
+    $('.surprise-backdrop, .surprise, .futureModal-backdrop, .futureModal').removeClass('open');
   });
 });
-
 
 
 
