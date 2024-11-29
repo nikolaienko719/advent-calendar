dffbac0f87599c453f0c781962a0115553aee490
nikolaienko19 <stiebastia@gmail.com>
2024-11-30 00:48:17 +0200

    uu

--- main.js
+++ main.js
@@ -92,128 +92,47 @@ $(document).ready(function () {
 const urlParams = new URLSearchParams(window.location.search);
 const doneTasksParam = urlParams.get('doneTasks');
 
-
-
 if (doneTasksParam) {
-  // Розбираємо масив doneTasks
-  const doneTasks = JSON.parse(decodeURIComponent(doneTasksParam));
-  console.log('Received doneTasks:', doneTasks);
+  try {
+    // Розбираємо масив doneTasks
+    const doneTasks = JSON.parse(decodeURIComponent(doneTasksParam));
+    console.log('Received doneTasks:', doneTasks);
 
-  // Викликаємо функцію обробки doneTasks
-  replaceDayBlock(doneTasks);
+    // Викликаємо функцію обробки doneTasks
+    replaceDayBlock(doneTasks);
+  } catch (error) {
+    console.error('Error parsing doneTasks:', error);
+  }
+} else {
+  console.log('No doneTasks provided');
 }
 
 // Функція заміни блоків
 function replaceDayBlock(doneTasks) {
-  if (doneTasks.includes(1)) {
-    const originalDiv = document.querySelector('.day1');
+  doneTasks.forEach((day) => {
+    const originalDiv = document.querySelector(`.day${day}`);
     if (originalDiv) {
-      const newDiv = createDoneDiv('day1done');
+      const newDiv = createDoneDiv(day);
       originalDiv.parentNode.replaceChild(newDiv, originalDiv);
     }
-  }
-  if (doneTasks.includes(2)) {
-    const originalDiv = document.querySelector('.day2');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day2done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(3)) {
-    const originalDiv = document.querySelector('.day3');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day3done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(4)) {
-    const originalDiv = document.querySelector('.day4');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day4done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(5)) {
-    const originalDiv = document.querySelector('.day5');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day5done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(6)) {
-    const originalDiv = document.querySelector('.day6');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day6done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(7)) {
-    const originalDiv = document.querySelector('.day7');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day7done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(8)) {
-    const originalDiv = document.querySelector('.day8');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day8done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(9)) {
-    const originalDiv = document.querySelector('.day9');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day9done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(10)) {
-    const originalDiv = document.querySelector('.day10');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day10done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(11)) {
-    const originalDiv = document.querySelector('.day11');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day11done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
-  if (doneTasks.includes(12)) {
-    const originalDiv = document.querySelector('.day12');
-    if (originalDiv) {
-      const newDiv = createDoneDiv('day12done');
-      originalDiv.parentNode.replaceChild(newDiv, originalDiv);
-    }
-  }
+  });
 }
 
 // Функція для створення нового блоку
-function createDoneDiv(doneClass) {
+function createDoneDiv(day) {
   const newDiv = document.createElement('div');
-  newDiv.classList.add('dayDone', doneClass);
+  newDiv.classList.add('dayDone', `day${day}done`);
 
   const img = document.createElement('img');
-  img.id = 'meme-img';
-  img.src = 'images/image_1.jpg';
-  img.alt = '';
-  img.width = '200';
-  img.aheight = '200';
+  img.id = `meme-img-day${day}`;
+  img.src = `images/image_${day}.jpg`; // Динамічний шлях до зображення
+  img.alt = `Зображення для дня ${day}`;
+  img.width = 200;
+  img.height = 200;
 
   newDiv.appendChild(img);
   return newDiv;
 }
 
-// Функція завантаження стандартної сторінки
-function loadDefaultPage() {
-  document.body.innerHTML = `
-    <div class="default-page">
-      <h1>No tasks completed</h1>
-      <p>Please complete some tasks to see progress here.</p>
-    </div>
-  `;
-}
+
 
